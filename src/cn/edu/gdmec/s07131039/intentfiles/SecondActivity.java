1cc695e681019cb7450af9405cc68e96c1eabee5
gdmec07131039 <943811502@qq.com>
2014-10-24 11:55:13 +0800

    intentfilter

---
+++ src/cn/edu/gdmec/s07131039/intentfiles/SecondActivity.java
@@ -0,0 +1,17 @@
+package cn.edu.gdmec.s07131039.intentfiles;
+
+import cn.edu.gdmec.s07131039.intentfiles.R;
+import cn.edu.gdmec.s07131039.intentfiles.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second);
+	}
+
+}
